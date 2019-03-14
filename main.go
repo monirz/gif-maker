defc5332beb267f8ac866e72037e19c40d319c8e
monirz <monir.smith@gmail.com>
2019-03-14 22:12:41 +0600

    demo website added

--- main.go
+++ main.go
@@ -5,17 +5,31 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"html/template"
 	"image"
 	"image/gif"
 	"image/jpeg"
+	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/nfnt/resize"
 )
+
+var tPath = "./temps/"
+var dPath = "./data/"
+
+var templateDirs = []string{"templates", "data"}
+var templates *template.Template
+
+func init() {
+	templates, _ = getTemplates()
+}
 
 type data struct {
 	Success Success `json:"success"`
@@ -26,19 +40,53 @@ type Success struct {
 	Message string
 }
 
+var (
+	status int
+)
+
 func main() {
-	http.HandleFunc("/create", fileUpload)
+	http.HandleFunc("/create", fileUploadHandler)
 	http.HandleFunc("/file", getFile)
+	http.HandleFunc("/upload", fileUploadHTML) //this is for the demo
+
+	http.HandleFunc("/", rootHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	fmt.Println("Listening on :8090...")
 	http.ListenAndServe(":8090", nil)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	data := map[string]string{
+		"title":  "gif maker",
+		"header": "My Header",
+		"footer": "My Footer",
+	}
+
+	outputHTML(w, data)
+
+}
+
+func outputHTML(w http.ResponseWriter, data map[string]string) {
+
+	if status == 201 {
+		data["status"] = "201"
+		data["url"] = "http://localhost:8090/file"
+	}
+	err := templates.ExecuteTemplate(w, "rootHTML", data)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func getFile(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "output.gif")
 }
 
-//fileUpload uploads nultiple files from formdata
-func fileUpload(w http.ResponseWriter, r *http.Request) {
+//fileUploadHandler uploads nultiple files from formdata
+func fileUploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	err := r.ParseMultipartForm(20000)
 
@@ -87,6 +135,52 @@ func fileUpload(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.Write([]byte(err.Error()))
 	}
+
+}
+
+func fileUploadHTML(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseMultipartForm(20000)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	formdata := r.MultipartForm
+
+	var files []*multipart.FileHeader
+	for k, v := range formdata.File {
+		fmt.Println(k, v)
+		files = v
+	}
+
+	d := formdata.Value
+	w.Header().Set("Content-Type", "application/json")
+
+	if len(d["delay"]) < 1 || len(files) < 1 {
+		w.WriteHeader(400)
+		w.Write([]byte(`{"error": "Delay field or files are required in formdata"}`))
+		return
+	}
+
+	di, err := strconv.Atoi(d["delay"][0])
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	//send file names and delay
+	err = createGif(files, di)
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	status = 201
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 
 }
 
@@ -191,4 +285,21 @@ func createGif(files []*multipart.FileHeader, delay int) error {
 
 	return nil
 
+}
+
+func getTemplates() (templates *template.Template, err error) {
+	var allFiles []string
+	for _, dir := range templateDirs {
+		files2, _ := ioutil.ReadDir(dir)
+		for _, file := range files2 {
+			filename := file.Name()
+			if strings.HasSuffix(filename, ".html") {
+				filePath := filepath.Join(dir, filename)
+				allFiles = append(allFiles, filePath)
+			}
+		}
+	}
+
+	templates, err = template.New("").ParseFiles(allFiles...)
+	return
 }
